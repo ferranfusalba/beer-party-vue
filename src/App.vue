2a5d22a44100ad6dabce8e17f84bb30c551218a7
ferranfusalba <ferran@protonmail.ch>
2022-02-01 15:40:55 +0100

    desktop responsive w/ bootstrap's col-size option

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <router-view />
+    <div class="row">
+      <div class="col-0 col-md-3"></div>
+      <div class="col-12 col-md-6"><router-view /></div>
+      <div class="col-0 col-md-3"></div>
+    </div>
   </div>
 </template>
 
