8849e319ff59bed99a6a0f14bcd0e2fd0b5eee76
ferranfusalba <ferran@protonmail.ch>
2022-02-01 11:09:29 +0100

    food pairing style update

--- src/views/Beer.vue
+++ src/views/Beer.vue
@@ -158,20 +158,15 @@
       <h5>Food pairing</h5>
       <!-- Card carousel -->
       <div
-        class="row flex-row flex-nowrap"
+        class="row flex-row flex-nowrap foodAlign"
         style="overflow-x: auto"
       >
         <!-- CARD 1 -->
         <div v-for="item in beerLocal.food_pairing"
-        :key="item" class="card" style="width: 18rem">
-          <img class="card-img-top" src="" alt="Card image cap" />
+        :key="item" class="card cardFood">
+          <img class="card-img-top" src="@/assets/eiliv-sonas-aceron-ZuIDLSz3XLg-unsplash.jpg" alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">{{ item }}</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
       </div>
