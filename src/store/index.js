0ea6a5667e9d2e17e41330e5fe399bd09d022314
ferranfusalba <ferran@protonmail.ch>
2022-02-03 17:32:11 +0100

    readme

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,20 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-    beers: [],
-  },
-  mutations: {
-    fillBeers(state, beersInAction) {
-      state.beers = beersInAction;
+    state: {
+        beers: [],
     },
-  },
-  actions: {
-    getBeers: async function ({ commit }) {
-      const data = await fetch("https://api.punkapi.com/v2/beers");
-      const beers = await data.json();
-      commit("fillBeers", beers);
+    mutations: {
+        fillBeers(state, beersInAction) {
+            state.beers = beersInAction;
+        },
     },
-  },
-  modules: {},
+    actions: {
+        getBeers: async function({ commit }) {
+            const data = await fetch("https://api.punkapi.com/v2/beers");
+            const beers = await data.json();
+            commit("fillBeers", beers);
+        },
+    },
+    modules: {},
 });
